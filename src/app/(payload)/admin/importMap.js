b6c6706829155b8fdd1b574a1f15fc4375e395f6
trio spire <shiv@trio-spire.com>
2026-09-10 13:55:07 +0530

    first commit

---
+++ src/app/(payload)/admin/importMap.js
@@ -0,0 +1,8 @@
+import { default as default_1f6a64c0ab3f4f1e5c9fb34fe2be6495 } from '../../../components/admin/Analytics'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
+
+/** @type import('payload').ImportMap */
+export const importMap = {
+  "/components/admin/Analytics#default": default_1f6a64c0ab3f4f1e5c9fb34fe2be6495,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+}
